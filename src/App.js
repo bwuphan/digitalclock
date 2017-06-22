ce0a268b66257751e2b2e0eb3a077a7190513222
Bobby <bwuphan@gmail.com>
2017-06-22 00:04:18 -0700

    Fix bug for 12am

--- src/App.js
+++ src/App.js
@@ -19,17 +19,15 @@ class App extends Component {
         var date = new Date()
         var meridian = 'am';
         var hours = date.getHours();
-        // console.log('hours',hours)
         var dateArray = date.toTimeString().split(" ")[0].split(':');
-        if (parseInt(hours) > 12) {
-          // console.log('HELLO')
+        if (parseInt(hours) === 0) {
+          dateArray[0] = '12';
+        } else if (parseInt(hours) > 12) {
           meridian = 'pm';
           dateArray[0] = (dateArray[0] - 12).toString();
         }
-        // console.log('WOOSADASDSA', date.toTimeString().split(" ")[0].split(':'))
         var dateString = dateArray.join(':') + meridian;
         this.setState({'time': dateString});
-        // console.log({time: date})
       }
       setInterval(function() {
         getTime();
